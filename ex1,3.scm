996d708a3e298591af6d88c6b83fced40e5636c4
Yoshua Wakeham <yoshwakeham@gmail.com>
2012-10-10 22:02:00 +1100

    Finally got that dang script working! Houston, we have lift off.

---
+++ ex1,3.scm
@@ -0,0 +1,6 @@
+(define (square x) (* x x))
+
+(define (sum-of-bigger-squares x y z)
+  (cond ((and (<= x y) (<= x z)) (+ (square y) (square z)))
+	((and (<= y z) (<= y x)) (+ (square x) (square z)))
+	((and (<= z x) (<= z y)) (+ (square x) (square y)))))
